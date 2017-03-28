9c32f21209f264f40ad62bde7810a2e4c2acc1f0
Travis CI <bobby.brennan@gmail.com>
2017-03-28 18:43:01 +0000

    Add page for discourse discussions

--- routes/discussion.js
+++ routes/discussion.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const pug = require('pug');
 const router = module.exports = new express.Router();
 
 const DISCOURSE_HOST = process.env.DISCOURSE_HOST || 'https://forum.kaltura.org';
@@ -8,32 +9,26 @@ function discourseURL(path) {
   return DISCOURSE_HOST + path + '?api_key=' + API_KEY;
 }
 
-function createPage(slug, title) {
-  title = encodeURIComponent(title);
-  return `
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-  This discussion doesn't exist yet. <a href="/discussion/${slug}/create?title=${title}">Create it now</a>.
-</body>
-</html>
-`
-}
+const newDiscussionTemplate = pug.compileFile(__dirname + '/../views/new_discussion.pug');
+const discussionTemplate = pug.compileFile(__dirname + '/../views/discussion.pug');
 
 router.use('/:slug', (req, res, next) => {
   let slug = req.slug = req.params.slug.toLowerCase().replace(/_/g, '');
   request.get(discourseURL('/tags/' + slug + '.json'), {json: true}, (err, resp, body) => {
     if (err) return res.status(500).send("Error");
-    if (res.statusCode >= 400 || body.errors) return next();
+    if (resp.statusCode >= 400 || body.errors) return next();
     let topic = body.topic_list.topics[0];
     if (!topic) return next();
-    res.redirect(discourseURL('/t/' + topic.id));
+    request.get(discourseURL('/t/' + topic.id + '.json'), {json: true}, (err, resp, body) => {
+      if (err) return res.status(500).send(err);
+      if (resp.statusCode >= 400 || body.errors) return res.status(resp.statusCode).send(JSON.stringify(body.errors));
+      res.send(discussionTemplate({discussion: body}))
+    })
   });
 })
 
 router.get('/:slug', (req, res) => {
-  return res.send(createPage(req.slug, req.query.title));
+  return res.send(newDiscussionTemplate({slug: req.params.slug, title: encodeURIComponent(req.query.title)}));
 });
 
 router.get('/:slug/create', (req, res) => {
