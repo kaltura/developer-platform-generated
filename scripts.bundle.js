c45f706b3912f2e467c22671de8c0d94f6aed986
Travis CI <bobby.brennan@gmail.com>
2019-01-23 16:26:58 +0000

    Build site

--- scripts.bundle.js
+++ scripts.bundle.js
@@ -4432,14 +4432,6 @@ for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=e.createElemen
 mixpanel.init("86f681be6799f4750b83c8c1509420b2");
 }
 
-;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-68472312-3', 'auto');
-ga('send', 'pageview');
-
 ;(function() {
   window.lucybot.tracker = function(name, properties) {
     if (window.lucybot.prerender) return;
